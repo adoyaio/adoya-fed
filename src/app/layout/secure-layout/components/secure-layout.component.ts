06fd145d981a56e98ae0329348ed3c4048c430c0
james <james@adoya.io>
2020-06-25 15:21:55 -0700

    progress

--- src/app/layout/secure-layout/components/secure-layout.component.ts
+++ src/app/layout/secure-layout/components/secure-layout.component.ts
@@ -1,18 +1,23 @@
 import { NavLinkService } from "./../services/nav-link.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
+import { MatSidenav } from "@angular/material";
 
 @Component({
   selector: "app-secure-layout",
   templateUrl: "./secure-layout.component.html",
-  styleUrls: ["./secure-layout.component.scss"]
+  styleUrls: ["./secure-layout.component.scss"],
 })
 export class SecureLayoutComponent implements OnInit {
+  @ViewChild("drawer", { static: true }) private drawer: MatSidenav;
   constructor(private router: Router, private navLinkService: NavLinkService) {}
 
   ngOnInit() {}
 
   toggleDrawer() {
-    this.navLinkService.setActiveLink(undefined);
+    this.drawer.toggle();
+  }
+  handleLogoClick() {
+    this.drawer.close();
   }
 }
