7bfa2f752b8a6a9cd7b9f2f5281b742f9ffc9fd8
james <james@adoya.io>
2020-07-03 16:33:54 -0700

    progress

--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from "@angular/common";
 import { AccountRoutingModule } from "./account-routing.module";
 import { AccountComponent } from "./components/account.component";
 import { MaterialModule } from "src/app/shared/material-design/material.module";
+import { DynamicFormModule } from "src/app/shared/dynamic-form/dynamic-form.module";
 
 @NgModule({
   declarations: [AccountComponent],
-  imports: [CommonModule, AccountRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    AccountRoutingModule,
+    MaterialModule,
+    DynamicFormModule,
+  ],
 })
 export class AccountModule {}
--- src/app/features/account/components/account.component.ts
+++ src/app/features/account/components/account.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AmplifyService } from "aws-amplify-angular";
+import { FormBuilder } from "@angular/forms";
+import { UserAccountService } from "src/app/core/services/user-account.service";
 
 @Component({
   selector: "app-account",
@@ -7,7 +9,50 @@ import { AmplifyService } from "aws-amplify-angular";
   styleUrls: ["./account.component.scss"],
 })
 export class AccountComponent implements OnInit {
-  constructor(private amplifyService: AmplifyService) {}
+  constructor(
+    private amplifyService: AmplifyService,
+    private fb: FormBuilder,
+    private userAccountService: UserAccountService
+  ) {}
 
-  ngOnInit() {}
+  accountForm = this.fb.group({
+    orgId: [""],
+    firstName: [""],
+    lastName: [""],
+    email: [""],
+  });
+
+  ngOnInit() {
+    const orgId = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "custom:org_id";
+      }).Value;
+
+    this.accountForm.get("orgId").setValue(orgId);
+
+    const firstName = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "custom:first_name";
+      }).Value;
+
+    this.accountForm.get("firstName").setValue(firstName);
+
+    const lastName = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "custom:last_name";
+      }).Value;
+
+    this.accountForm.get("lastName").setValue(lastName);
+
+    const email = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "email";
+      }).Value;
+
+    this.accountForm.get("email").setValue(email);
+  }
 }
