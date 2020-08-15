541ad9d7490b68da7c42e77b4d7e5da8290cb38f
James <jarfarri@gmail.com>
2020-08-14 18:26:58 -0700

    data vis

--- src/app/features/reporting/components/line-chart/line-chart.component.ts
+++ src/app/features/reporting/components/line-chart/line-chart.component.ts
@@ -1,34 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { Component, OnInit, Input } from "@angular/core";
+import { ChartDataSets, ChartOptions, ChartType } from "chart.js";
+import { Color, Label } from "ng2-charts";
+import { CostPerInstallDayObject } from "../../models/cost-per-install-day-object";
 
 @Component({
-  selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.css']
+  selector: "app-line-chart",
+  templateUrl: "./line-chart.component.html",
+  styleUrls: ["./line-chart.component.css"],
 })
 export class LineChartComponent implements OnInit {
+  @Input() dataSource: CostPerInstallDayObject[];
+  @Input() displayedColumns: string[];
 
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
   ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels: Label[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
   public lineChartOptions: ChartOptions = {
     responsive: true,
   };
   public lineChartColors: Color[] = [
     {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: "black",
+      backgroundColor: "rgba(255,0,0,0.3)",
     },
   ];
   public lineChartLegend = true;
-  public lineChartType: ChartType = 'line';
+  public lineChartType: ChartType = "line";
   public lineChartPlugins = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.lineChartLabels = this.displayedColumns;
   }
-
 }
