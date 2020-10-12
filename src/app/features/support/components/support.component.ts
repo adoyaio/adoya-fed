83e9c0c8e715f62862327c3c1cfe1f1267112a14
James <jarfarri@gmail.com>
2020-10-12 16:09:52 -0700

    support-page

--- src/app/features/support/components/support.component.ts
+++ src/app/features/support/components/support.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AmplifyService } from "aws-amplify-angular";
+import { tap } from "rxjs/operators";
+import { ClientService } from "src/app/core/services/client.service";
+import { UserAccountService } from "src/app/core/services/user-account.service";
+import { SupportItem } from "../models/support-item";
 
 @Component({
-  selector: 'app-support',
-  templateUrl: './support.component.html',
-  styleUrls: ['./support.component.scss']
+  selector: "app-support",
+  templateUrl: "./support.component.html",
+  styleUrls: ["./support.component.scss"],
 })
 export class SupportComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userAccountService: UserAccountService,
+    private amplifyService: AmplifyService
+  ) {}
 
-  constructor() { }
+  supportForm = this.fb.group({
+    subject: [""],
+    description: [""],
+    username: [""],
+    orgId: [""],
+  });
+
+  client: SupportItem = new SupportItem();
+  isLoadingResults = true;
+  isSendingResults;
+  orgId: string;
+  subject: string;
+  username: string;
 
   ngOnInit() {
-  }
+    this.amplifyService
+      .authState()
+      .pipe(
+        tap((authState) => {
+          if (!(authState.state === "signedIn")) {
+            this.router.navigateByUrl("/portal");
+          }
+        })
+      )
+      .subscribe();
 
+    this.orgId = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "custom:org_id";
+      }).Value;
+
+    this.username = this.userAccountService
+      .getCurrentUser()
+      .UserAttributes.find((val) => {
+        return val.Name === "email";
+      }).Value;
+
+    this.subject = "10/15/25: Adoya support ticket";
+    this.username = "jakub@weareher.com";
+    this.supportForm.get("subject").disable();
+    this.supportForm.get("subject").setValue(this.subject);
+
+    this.supportForm.get("username").disable();
+    this.supportForm.get("username").setValue(this.username);
+
+    this.supportForm.get("orgId").disable();
+    this.supportForm.get("orgId").setValue(this.orgId);
+
+    this.supportForm
+      .get("description")
+      .setValidators([Validators.minLength(1), Validators.required]);
+  }
+  onSupportSubmit() {}
+  onResetForm() {}
 }
--- src/app/features/support/support.module.ts
+++ src/app/features/support/support.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from "@angular/common";
 import { SupportRoutingModule } from "./support-routing.module";
 import { SupportComponent } from "./components/support.component";
 import { MaterialModule } from "src/app/shared/material-design/material.module";
+import { DynamicFormModule } from "src/app/shared/dynamic-form/dynamic-form.module";
 
 @NgModule({
   declarations: [SupportComponent],
-  imports: [CommonModule, SupportRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    SupportRoutingModule,
+    MaterialModule,
+    DynamicFormModule,
+  ],
 })
 export class SupportModule {}
