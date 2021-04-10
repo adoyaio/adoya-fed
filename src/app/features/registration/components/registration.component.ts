3d39b5c1ddcb4750dc78dd2912043636cfcc4919
James <jarfarri@gmail.com>
2021-04-09 21:42:20 -0700

    registration

--- src/app/features/registration/components/registration.component.ts
+++ src/app/features/registration/components/registration.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.scss"],
 })
 export class RegistrationComponent implements OnInit {
+  countries = ["United States", "Canada", "Japan"];
+  apps = ["App ABC", "App ABC", "App ABC"];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
