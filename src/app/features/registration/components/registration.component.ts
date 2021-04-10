205df555ee0f9780495998280ed4595a06b36467
James <jarfarri@gmail.com>
2021-04-10 15:29:47 -0700

    registration

--- src/app/features/registration/components/registration.component.ts
+++ src/app/features/registration/components/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { chain, find, get, set } from "lodash";
 
 @Component({
   selector: "app-registration",
@@ -7,8 +8,38 @@ import { Component, OnInit } from "@angular/core";
 })
 export class RegistrationComponent implements OnInit {
   countries = ["United States", "Canada", "Japan"];
-  apps = ["App ABC", "App ABC", "App ABC"];
+  apps = ["App ABC", "App ABC", "App ABC"]; // TODO load from api
+  step2: any[] = [
+    { ordinal: 1, complete: false, active: true },
+    { ordinal: 2, complete: false, active: false },
+    { ordinal: 3, complete: false, active: false },
+  ];
   constructor() {}
 
   ngOnInit() {}
+
+  isOrdinalActive(ordinal: number): boolean {
+    const step = find(this.step2, (step) => step.ordinal === ordinal);
+    return get(step, "active", false);
+  }
+
+  submitStep(ordinal: number) {
+    const step = find(this.step2, (step) => step.ordinal === ordinal);
+    set(step, "complete", true);
+    set(step, "active", false);
+
+    const nextStep = find(this.step2, (step) => step.ordinal === ordinal + 1);
+    set(nextStep, "complete", false);
+    set(nextStep, "active", true);
+  }
+
+  goBack(ordinal: number) {
+    const step = find(this.step2, (step) => step.ordinal === ordinal);
+    set(step, "complete", true);
+    set(step, "active", false);
+
+    const prevStep = find(this.step2, (step) => step.ordinal === ordinal - 1);
+    set(prevStep, "complete", false);
+    set(prevStep, "active", true);
+  }
 }
