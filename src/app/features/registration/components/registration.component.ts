dd3a13a58653654717d6af6ea65be1e04d732468
James <jarfarri@gmail.com>
2021-04-11 10:58:18 -0700

    registration

--- src/app/features/registration/components/registration.component.ts
+++ src/app/features/registration/components/registration.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
+import { Router } from "@angular/router";
+import { AmplifyService } from "aws-amplify-angular";
 import { chain, find, get, set } from "lodash";
+import { Client } from "src/app/core/models/client";
+import { ClientService } from "src/app/core/services/client.service";
+import { UserAccountService } from "src/app/core/services/user-account.service";
 
 @Component({
   selector: "app-registration",
@@ -10,12 +17,69 @@ export class RegistrationComponent implements OnInit {
   countries = ["United States", "Canada", "Japan"];
   apps = ["App ABC", "App ABC", "App ABC"]; // TODO load from api
   step2: any[] = [
-    { ordinal: 1, complete: false, active: true },
-    { ordinal: 2, complete: false, active: false },
-    { ordinal: 3, complete: false, active: false },
-    { ordinal: 4, complete: false, active: false },
+    {
+      ordinal: 1,
+      complete: false,
+      active: true,
+      title: "Select Application and Country",
+    },
+    {
+      ordinal: 2,
+      complete: false,
+      active: false,
+      title: "Objective and CPI Goals",
+    },
+    {
+      ordinal: 3,
+      complete: false,
+      active: false,
+      title: "Lifetime and Daily Budget",
+    },
+    {
+      ordinal: 4,
+      complete: false,
+      active: false,
+      title: "List Competitors and Brands",
+    },
+    {
+      ordinal: 5,
+      complete: false,
+      active: false,
+      title: "Genders and age groups",
+    },
+    {
+      ordinal: 6,
+      complete: false,
+      active: false,
+      title: "Post Install Optimizations (Optional)",
+    },
   ];
-  constructor() {}
+  constructor(
+    private amplifyService: AmplifyService,
+    private router: Router,
+    private fb: FormBuilder,
+    private userAccountService: UserAccountService,
+    private clientService: ClientService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  appleForm = this.fb.group({
+    objective: [""],
+    cpi: [""],
+  });
+  branchForm = this.fb.group({
+    mmpObjective: [""],
+    cpp: [""],
+    roas: [""],
+    branchBidAdjusterEnabled: false,
+    branchKey: [""],
+    branchSecret: [""],
+  });
+
+  client: Client = new Client();
+  isLoadingResults = true;
+  isSendingResults;
+  orgId: string;
 
   ngOnInit() {}
 
@@ -43,4 +107,6 @@ export class RegistrationComponent implements OnInit {
     set(prevStep, "complete", false);
     set(prevStep, "active", true);
   }
+
+  handleBranchCheckboxChange() {}
 }
