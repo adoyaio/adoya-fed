64b256df31f50cfa501256d8e8bf1b045418bca0
James <jarfarri@gmail.com>
2021-04-24 15:09:03 -0700

    registration

--- src/app/features/registration/components/registration.component.ts
+++ src/app/features/registration/components/registration.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 import { MatSnackBar } from "@angular/material";
 import { Router } from "@angular/router";
 import { AmplifyService } from "aws-amplify-angular";
-import { chain, find, get, set } from "lodash";
+import { chain, find, get, isNil, set } from "lodash";
 import { Client } from "src/app/core/models/client";
 import { ClientService } from "src/app/core/services/client.service";
 import { UserAccountService } from "src/app/core/services/user-account.service";
@@ -58,11 +63,23 @@ export class RegistrationComponent implements OnInit {
     },
     {
       ordinal: 6,
-      complete: false,
+      complete: true,
       active: false,
       title: "Post Install Optimizations (Optional)",
     },
   ];
+
+  // get substep1(): any {
+  //   return this.step2Form.get("substep1");
+  // }
+  // get application(): any {
+  //   return this.step2Form.get("substep1.application");
+  // }
+
+  // get country(): any {
+  //   return this.step2Form.get("substep1.country");
+  // }
+
   constructor(
     private amplifyService: AmplifyService,
     private router: Router,
@@ -71,19 +88,6 @@ export class RegistrationComponent implements OnInit {
     private clientService: ClientService,
     private snackBar: MatSnackBar
   ) {}
-
-  // appleForm = this.fb.group({
-  //   objective: [""],
-  //   cpi: [""],
-  // });
-  // branchForm = this.fb.group({
-  //   mmpObjective: [""],
-  //   cpp: [""],
-  //   roas: [""],
-  //   branchBidAdjusterEnabled: false,
-  //   branchKey: [""],
-  //   branchSecret: [""],
-  // });
 
   step1Form = this.fb.group({
     clientId: [""],
@@ -94,14 +98,35 @@ export class RegistrationComponent implements OnInit {
   });
 
   step2Form = this.fb.group({
-    objective: [""],
-    cpi: [""],
-    mmpObjective: [""],
-    cpp: [""],
-    roas: [""],
-    branchBidAdjusterEnabled: false,
-    branchKey: [""],
-    branchSecret: [""],
+    substep1: this.fb.group({
+      application: new FormControl(undefined, Validators.required),
+      country: new FormControl(undefined, Validators.required),
+    }),
+    substep2: this.fb.group({
+      objective: new FormControl(undefined, Validators.required),
+      cpi: new FormControl(undefined, Validators.required),
+    }),
+    substep3: this.fb.group({
+      dailyBudget: new FormControl(undefined, Validators.required),
+      lifetimeBudget: new FormControl(undefined, Validators.required),
+    }),
+    substep4: this.fb.group({
+      competitors: new FormControl(undefined, Validators.required),
+      phrases: new FormControl(undefined, Validators.required),
+      brand: new FormControl(undefined, Validators.required),
+    }),
+    substep5: this.fb.group({
+      genders: new FormControl(undefined, Validators.required),
+      ages: new FormControl(undefined, Validators.required),
+    }),
+    substep6: this.fb.group({
+      mmpObjective: new FormControl(undefined),
+      cpp: new FormControl(undefined),
+      roas: new FormControl(undefined),
+      branchBidAdjusterEnabled: new FormControl(false),
+      branchKey: new FormControl(false),
+      branchSecret: new FormControl(false),
+    }),
   });
 
   client: Client = new Client();
@@ -137,4 +162,41 @@ export class RegistrationComponent implements OnInit {
   }
 
   handleBranchCheckboxChange() {}
+
+  substepDisabled(): boolean {
+    const activeSubstep = find(this.step2, (step) => step.active === true);
+
+    if (activeSubstep.ordinal === 1) {
+      return this.step2Form.get("substep1").invalid;
+    }
+
+    if (activeSubstep.ordinal === 2) {
+      return this.step2Form.get("substep2").invalid;
+    }
+
+    if (activeSubstep.ordinal === 3) {
+      return this.step2Form.get("substep3").invalid;
+    }
+
+    if (activeSubstep.ordinal === 4) {
+      return this.step2Form.get("substep4").invalid;
+    }
+
+    if (activeSubstep.ordinal === 5) {
+      return this.step2Form.get("substep5").invalid;
+    }
+
+    return false;
+  }
+
+  // stepDisabled(formName: string): boolean {
+  //   const formGroup: FormGroup = get(this, formName);
+  //   // console.log(JSON.stringify(formGroup));
+  //   return formGroup.invalid;
+  //   // return false;
+  // }
+
+  get substep1(): any {
+    return this.step2Form.get("substep1");
+  }
 }
