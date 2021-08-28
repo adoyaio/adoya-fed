1dac2419b25770bbc691d40ac41650ebd22635aa
James <jarfarri@gmail.com>
2021-08-28 14:58:10 -0700

    carousel

--- src/app/features/splash/splash.component.ts
+++ src/app/features/splash/splash.component.ts
@@ -14,4 +14,21 @@ export class SplashComponent implements OnInit {
     { image: "../../../assets/images/reporting-1.png" },
     { image: "../../../assets/images/reporting-7.png" },
   ];
+
+  optimizeSlides = [
+    { image: "../../../assets/images/optimize-1.png" },
+    { image: "../../../assets/images/optimize-4.png" },
+    { image: "../../../assets/images/optimize-5.png" },
+    { image: "../../../assets/images/optimize-6.png" },
+  ];
+
+  visulizeSlides = [
+    { image: "../../../assets/images/reporting-2.png" },
+    { image: "../../../assets/images/reporting-3.png" },
+  ];
+
+  integrationSlides = [
+    { image: "../../../assets/images/integration-1.png" },
+    { image: "../../../assets/images/integration-2.png" },
+  ];
 }
