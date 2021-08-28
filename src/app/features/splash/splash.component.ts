491bf4895f856219bdb9df987d63a43070654b37
James <jarfarri@gmail.com>
2021-08-28 14:30:12 -0700

    carousel

--- src/app/features/splash/splash.component.ts
+++ src/app/features/splash/splash.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-splash',
-  templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss']
+  selector: "app-splash",
+  templateUrl: "./splash.component.html",
+  styleUrls: ["./splash.component.scss"],
 })
 export class SplashComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  reportingSlides = [
+    { image: "../../../assets/images/reporting-1.png" },
+    { image: "../../../assets/images/reporting-7.png" },
+  ];
 }
